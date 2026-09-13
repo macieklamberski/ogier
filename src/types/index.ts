3ab1acd8c1703786b2a923ec094159368782ca23
Maciej Lamberski <maciej@lamberski.com>
2026-09-13 16:06:42 +0200

    feat: Export the types from one barrel

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type {
+  Background,
+  Card,
+  FontRole,
+  Fonts,
+  IconRef,
+  ImageRef,
+  SizeOverrides,
+  Sizes,
+  Theme,
+} from './card.js'
+export type { Icon, Layout, LayoutContext, Node } from './layout.js'
+export type { Metadata, MetadataImage, MetadataInput, MetaTag } from './metadata.js'
+export type { RenderOptions } from './render.js'
