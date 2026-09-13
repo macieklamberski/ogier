7d7404c439136b1a4ee013ecab32bfdbc96cb158
Maciej Lamberski <maciej@lamberski.com>
2026-09-13 08:33:54 +0200

    feat: Add the metadata types

---
+++ src/types/metadata.ts
@@ -0,0 +1,25 @@
+export type MetadataImage = {
+  url: string
+  width: number
+  height: number
+  alt: string
+}
+
+export type Metadata = {
+  type: string
+  url: string
+  title: string
+  description?: string
+  image: MetadataImage
+  twitterCard: 'summary_large_image'
+}
+
+export type MetadataInput = {
+  type?: string
+  url: string
+  title: string
+  description?: string
+  image: string | Partial<MetadataImage>
+}
+
+export type MetaTag = ['meta', Record<string, string>]
