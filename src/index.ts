cd2b84a5d7d087a7967f62aeef3ead6de2b2f764
Maciej Lamberski <maciej@lamberski.com>
2026-09-13 22:04:19 +0200

    feat: Export the public surface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-export {}
+export type { DocsSizes } from './layouts/docs.js'
+export { docsLayout, docsSizes } from './layouts/docs.js'
+export { getImageUrl, getMetadata, toMetaTags } from './metadata.js'
+export { renderPng, renderSvg } from './render.js'
+export { darkTheme } from './themes/dark.js'
+export type {
+  Background,
+  Card,
+  FontRole,
+  Fonts,
+  Icon,
+  IconRef,
+  ImageRef,
+  Layout,
+  LayoutContext,
+  Metadata,
+  MetadataImage,
+  MetadataInput,
+  MetaTag,
+  Node,
+  RenderOptions,
+  SizeOverrides,
+  Sizes,
+  Theme,
+} from './types/index.js'
